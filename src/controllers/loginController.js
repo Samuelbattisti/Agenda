b29882dddd34025dd71ae43db44b7f099f3260c9
Samuel <samuelbattisti10@gmail.com>
2024-01-08 11:07:40 -0300

    creating success messages when creating user correctly

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -4,16 +4,25 @@ exports.index = (req, res) => {
     res.render('login')
 };
 
-exports.register = async function(req, res) {
-    const login = new Login(req.body);
-    await login.register();
-    
-    if(login.errors.length > 0 ){
-        req.flash('errors', login.errors);
-        req.session.save(function(){
-           return res.redirect('/login/index');
+exports.register = async function (req, res) {
+    try {
+        const login = new Login(req.body);
+        await login.register();
+
+        if (login.errors.length > 0) {
+            req.flash('errors', login.errors);
+            req.session.save(function () {
+                return res.redirect('/login/index');
+            });
+            return;
+        }
+        req.flash('success', 'Seu usuário foi criado com sucesso!');
+        req.session.save(function () {
+            return res.redirect('/login/index');
         });
-        return;
+    } catch (e) {
+        console.log(e)
+        res.render('404')
     }
 
 
