76a2c488181025df4bdc295d355be0c7e47da2b8
Samuel <samuelbattisti10@gmail.com>
2024-01-09 11:15:02 -0300

    creating contact register

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,7 +1,8 @@
 const Login = require('../models/LoginModel');
 
 exports.index = (req, res) => {
-    res.render('login')
+    if(req.session.user) return res.render('login-logged')
+    return res.render('login')
 };
 
 exports.register = async function (req, res) {
@@ -16,7 +17,7 @@ exports.register = async function (req, res) {
             });
             return;
         }
-        req.flash('success', 'Seu usuário foi criado com sucesso!');
+        req.flash('success', 'Usuário criado com sucesso!');
         req.session.save(function () {
             return res.redirect('/login/index');
         });
@@ -25,5 +26,32 @@ exports.register = async function (req, res) {
         res.render('404')
     }
 
+};
+exports.login = async function (req, res) {
+    try {
+        const login = new Login(req.body);
+        await login.login();
+
+        if (login.errors.length > 0) {
+            req.flash('errors', login.errors);
+            req.session.save(function () {
+                return res.redirect('/login/index');
+            });
+            return;
+        }
+        req.flash('success', 'Login efetuado com sucesso!');
+        req.session.user = login.user;
+        req.session.save(function () {
+            return res.redirect('/login/index');
+        });
+    } catch (e) {
+        console.log(e);
+        return res.render('404');
+    }
 
 };
+
+exports.logout = function(req,res){
+    req.session.destroy();
+    res.redirect('/');
+}
--- src/middlewares/middleware.js
+++ src/middlewares/middleware.js
@@ -1,6 +1,7 @@
 exports.middlewareGlobal = (req, res, next) => {
     res.locals.errors = req.flash('errors');
     res.locals.success = req.flash('success');
+    res.locals.user = req.session.user;
     next();
   };
   
@@ -17,6 +18,15 @@ exports.middlewareGlobal = (req, res, next) => {
   
   exports.csrfMiddleware = (req, res, next) => {
     res.locals.csrfToken = req.csrfToken();
+    next();
+  };
+    
+  exports.loginRequired = (req, res, next) => {
+    if(!req.session.user){
+      req.flash('errors', 'Você precisa fazer login para cadastrar seus contatos!');
+      req.session.save(() => res.redirect ('/'));
+      return;
+    }
     next();
   };
   
